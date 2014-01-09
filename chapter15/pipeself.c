83ed9af6f5c558acd4200b592e3914c9bd8adf1e
spch2008 <spch2008@foxmail.com>
2014-01-09 06:05:36 -0500

    read fd closed, write generate SIGPIPE

--- chapter15/pipeself.c
+++ chapter15/pipeself.c
@@ -2,34 +2,43 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
 
+
+static void sig_pipe(int);
 
 int
 main(int argc, char *argv[])
 {
-    int fd[2], res;
+    int fd[2];
     char buf[20];    
-   
+    
+    if (signal(SIGPIPE, sig_pipe) < 0) {
+        perror("signal error!\n");
+        exit(0);
+    }
 
     if (pipe(fd) < 0) {
         perror("create pipe error !\n");
         exit(0);
 
     }
-    close(fd[1]);
-    //strcpy(buf, "hello world");
-    //write(fd[1], buf, strlen(buf));
     
-    memset(buf, 0, sizeof(buf));
-    res = read(fd[0], buf, sizeof(buf));
-    if (res == 0) {
-         printf("write fd closed\n");
+    close(fd[0]);
+     
+    strcpy(buf, "hello world");
+    write(fd[1], buf, strlen(buf));
+    
+    //memset(buf, 0, sizeof(buf));
+    //read(fd[0], buf, sizeof(buf));
+    //printf("read data : %s\n", buf);
 
-    } else {
-        printf("read data : %s\n", buf);
-
-    }
 
     return 0;
 }
 
+static void 
+sig_pipe(int sig)
+{
+    printf("caught SIGPIPE\n");
+}
